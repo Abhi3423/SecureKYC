c57a28608dfaa4d9d24a8ef6a64b4257d26c97da
Aditya Mittal <adityamittal2410@gmail.com>
2024-03-14 02:41:16 +0530

    Revert "feat: added document scanner"
    This reverts commit b8f788fcf60192e92cebdca7351e00d78f05fcb0.

--- frontend/src/components/KYC/DocumentScanning.jsx
+++ frontend/src/components/KYC/DocumentScanning.jsx
@@ -3,42 +3,28 @@ import Webcam from "react-webcam";
 
 const DocumentScanner = () => {
   const webcamRef = useRef(null);
-  const [screenshots, setScreenshots] = useState([]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      captureScreenshot();
-    }, 3000); // Capture screenshot every 3 seconds
-
-    // Stop capturing after 20 seconds
-    setTimeout(() => {
-      clearInterval(interval);
-    }, 20000);
-
-    return () => clearInterval(interval); // Cleanup function
-  }, []);
-
-  const captureScreenshot = () => {
+  let screenshotImage = "";
+  const showImage = () => {
     const imageSrc = webcamRef.current.getScreenshot();
-    setScreenshots((prevScreenshots) => {
-      const newScreenshots = [...prevScreenshots, imageSrc];
-      console.log(newScreenshots); // Log the array when a new element is added
-      return newScreenshots;
-    });
+    screenshotImage = imageSrc;
+    console.log(screenshotImage);
   };
   return (
     <article className="rounded-xl border border-blue-700 bg-gray-100 p-4 m-4 mt-11">
       <div className="rounded-lg border border-blue-700 p-4 flex items-center justify-between">
         <Webcam ref={webcamRef} />
       </div>
-      <button className="bg-gray-300 p-3 m-2 border-black rounded-md hover:bg-gray-400">
+      <button
+        className="bg-gray-300 p-3 m-2 border-black rounded-md hover:bg-gray-400"
+        onClick={() => showImage()}
+      >
         Show Image in console
       </button>
-      {/* this paragraph will be dynamic to render the instructions for the document  */}
-      <div className="flex flex-wrap rounded-xl bg-gray-300 max-w-sm mx-auto mt-8 p-3">
+      <div className="flex flex-wrap rounded-xl bg-gray-300 max-w-sm mx-auto mt-8">
         Please show your Adhaar card to the camera. Make sure that the card is
         properly visible and the details are clear.
       </div>
+      <img src={screenshotImage} alt="" className=" w-36 h-36" />
     </article>
   );
 };
