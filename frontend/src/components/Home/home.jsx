2b40e28f59728cb15745985e6343c7c2104cdb18
Arnav Shukla <87523233+thelordzeus@users.noreply.github.com>
2024-03-13 21:18:38 +0530

    feat: updated graphic on not found page

--- frontend/src/components/Home/home.jsx
+++ frontend/src/components/Home/home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import { DataContext } from "../../shared/containers/provider";
-import { Fragment,useContext } from 'react';
+import { Fragment, useContext } from "react";
 
 const Home = () => {
   const { data } = useContext(DataContext);
   return (
     <Fragment>
-      <div className='text-black font-bold text-4xl'>Home</div>
+      <div className="text-black font-bold text-4xl">Home</div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
