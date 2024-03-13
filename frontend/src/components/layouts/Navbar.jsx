ccb80f278a58306deb9e45310faca4ad236c8676
Arnav Shukla <87523233+thelordzeus@users.noreply.github.com>
2024-03-13 21:58:50 +0530

    minor navbar changes

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <header className="bg-white">
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8 p-2">
         <a className="block text-teal-600" href="#">
           <span className="sr-only">Home</span>
           <div className="w-16 h-14">
@@ -75,13 +75,8 @@ const Navbar = () => {
           </nav>
 
           <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
-              <a
-                className="block rounded-md bg-blue-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700"
-                href="#"
-              >
-                Login
-              </a>
+            <div className="sm:flex sm:gap-4 w-16 h-14">
+              <img src="/kyclogo.png" alt="KYC logo" className="w-16 h-14" />
             </div>
           </div>
         </div>
